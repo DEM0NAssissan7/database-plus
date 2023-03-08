f935379e0f9f12d532f4e46dd439b77a097a9760
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-03-08 12:25:14 -0600

    add average calculations

--- dbplus.js
+++ dbplus.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  A nice upgrade to the peace database
 // @author       You
-// @match        http://peaceacademy.net/
+// @match        http://peaceacademy.net/*
 // @icon         https://static.toiimg.com/thumb/msid-51767839,imgsize-17046,width-400,resizemode-4/51767839.jpg
 // @grant        none
 // ==/UserScript==
@@ -12,5 +12,46 @@
 (function() {
     'use strict';
 
-    
+    function get_path(path) {
+        return document.querySelector(path);
+    }
+
+    // Student grades
+    function get_student_grade() {
+        let result = 0;
+        let element;
+        let i = 0;
+        while (true) {
+            element = get_path("#ContentPlaceHolder1_GridView1 > tbody > tr:nth-child("+ (i + 2) + ")");
+            if(element === null) break;
+            result += parseFloat(element.childNodes[6].innerText);
+            i++
+        }
+        result = result / i;
+        return result;
+    }
+    console.log(get_avg_student_grade());
+    console.log(get_avg_student_grade() / 25);
+
+    // Class grades
+    function get_class_grade() {
+        let result = 0;
+        let weight_sum = 0;
+        let element;
+        let i = 0;
+        while (true) {
+            element = get_path("#ContentPlaceHolder1_GridView2 > tbody > tr:nth-child("+ (i + 2) + ")");
+            if(element === null) break;
+            let numerator = parseFloat(element.childNodes[5].innerText);
+            let denominator = parseFloat(element.childNodes[7].innerText);
+            weight_sum += parseFloat(element.childNodes[6].innerText);
+
+            result += numerator / denominator;
+            weight_sum += weight;
+            i++
+        }
+        result = result / i;
+        return result;
+    }
+
 })();
