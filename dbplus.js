6aa61caa2f3445480464b7a8df727a07d4713107
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-03-08 11:57:22 -0600

    initial commit

---
+++ dbplus.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Database Plus
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  A nice upgrade to the peace database
+// @author       You
+// @match        http://peaceacademy.net/
+// @icon         https://static.toiimg.com/thumb/msid-51767839,imgsize-17046,width-400,resizemode-4/51767839.jpg
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    
+})();
