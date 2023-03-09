14a804b6eb2bda53bd12817c8188d268e38e7d60
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-03-08 23:29:53 -0600

    version 1.3

--- dbplus.js
+++ dbplus.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Database+
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  A nice upgrade to the Peace Database
 // @author       Abdurahman Elmawi
 // @match        http://peaceacademy.net/*
