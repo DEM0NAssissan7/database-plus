1767f97cbc7f5a1bf1e6a6df75e4388936030587
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2023-03-31 00:12:40 -0500

    intial libdb

---
+++ libdb.js
@@ -0,0 +1,17 @@
+/* School grade database processing and display library
+
+The goal of this project is to provide a standard where any school grade database can enjoy the features of database+.
+This is for me when I go to college God willing, and the grades websites may suck, so I have to recode database+ for 
+every website I want to use it.
+
+Instead, this project will standardize all calculations, displays, features, and designs. All you would need to do to port it to
+a new website is implement it and tell libdb what the grades/weights (and other things) are.
+
+The end goal is to port peace academy db+ to this library in order for it to become monolithic and portable.
+
+"Portability! Portability! Portability!" - Stoove Jeebs
+*/
+
+{
+    
+}
